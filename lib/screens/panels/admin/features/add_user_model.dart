acd561c2cac1bfc7fcd00829a5f75202026341e4
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-04-09 14:46:15 +0200

    refactor: add scroll capability

--- lib/screens/panels/admin/features/add_user_model.dart
+++ lib/screens/panels/admin/features/add_user_model.dart
@@ -13,63 +13,65 @@ class AddUserModal extends StatelessWidget {
 
     return Dialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(DAppSizes.radius)),
-      child: Padding(
-        padding: const EdgeInsets.all(DAppSizes.spacingMedium),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Add New User", style: Theme.of(context).textTheme.titleMedium),
-                IconButton(
-                    onPressed: () {Navigator.pop(context);}, icon: Icon(Icons.close)
-                )
-              ],
-            ),
-            Theme(
-                data: Theme.of(context).copyWith(
-                  inputDecorationTheme: DThemes.inputDecorationThemePadded,
-                ), 
-                child: Form(
-                    child: Column(
-                      spacing: DAppSizes.spacingMedium,
-                      children: <Widget>[
-                        TextFormField(
-                          decoration: InputDecoration(
-                              hintText: "Enter full name"
-                          ),
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              hintText: "Enter Email address"
-                          ),
-                        ),
-                        IntlPhoneField(
-                            initialCountryCode: initialCountry,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(DAppSizes.spacingMedium),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Add New User", style: Theme.of(context).textTheme.titleMedium),
+                  IconButton(
+                      onPressed: () {Navigator.pop(context);}, icon: Icon(Icons.close)
+                  )
+                ],
+              ),
+              Theme(
+                  data: Theme.of(context).copyWith(
+                    inputDecorationTheme: DThemes.inputDecorationThemePadded,
+                  ),
+                  child: Form(
+                      child: Column(
+                        spacing: DAppSizes.spacingMedium,
+                        children: <Widget>[
+                          TextFormField(
                             decoration: InputDecoration(
-                                hintText: '791154300'
-                            )
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              hintText: "Facility"
+                                hintText: "Enter full name"
+                            ),
                           ),
-                        ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                hintText: "Enter Email address"
+                            ),
+                          ),
+                          IntlPhoneField(
+                              initialCountryCode: initialCountry,
+                              decoration: InputDecoration(
+                                  hintText: '791154300'
+                              )
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                hintText: "Facility"
+                            ),
+                          ),
 
-                        SizedBox(
-                          height: 56.0,
-                          width: double.infinity,
-                          child: ElevatedButton(
-                              onPressed: () => {},
-                              child: Text(DTexts.addUserBtn)
-                          ),
-                        )
-                      ],
-                    )
-                )
-            )
-          ],
+                          SizedBox(
+                            height: DAppSizes.buttonHeight,
+                            width: double.infinity,
+                            child: ElevatedButton(
+                                onPressed: () => {},
+                                child: Text(DTexts.addUserBtn)
+                            ),
+                          )
+                        ],
+                      )
+                  )
+              )
+            ],
+          ),
         ),
       ),
     );
